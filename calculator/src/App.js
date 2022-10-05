353e451f6302d7affd8382f71bb4914696747d1e
Raheema Jannah <ed202rj@leeds.ac.uk>
2022-10-05 18:12:38 +0100

    began adding some functionality

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,31 +1,49 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 
+//need to do all the operator buttons,
+//and fix when a new digit is added after AC button
+//remember after the equals sign has been pressed you want to be able to update the previous calculation
 function App(){
+    const[currentCalculation, setCurrentCalculation] = useState("")
+    const[previousCalculation, setPreviousCalculation] = useState("")
+
+    const Addigits = (newDigit) => {
+        setCurrentCalculation(currentCalculation + newDigit);
+    }
+
+    const clear = () => {
+        setCurrentCalculation(0);
+    }
+
+    const deleteLastDigit = () => {
+        setCurrentCalculation(currentCalculation.slice(0, -1))
+    }
+
     return (
-        <div className="calculatorGrid"> 
-            <div className="outputBox">
-                <div className="previousValue">123,400</div>
-                <div className="currentValue">6+5</div>
+        <div className="calculator"> 
+            <div className="outputPanel">
+                <div className="previousCalculation">{previousCalculation || null}</div>
+                <div className="currentCalculation">{currentCalculation || "0"}</div> 
             </div>
-            <button className="largerButtons">AC</button>
-            <button>DEL</button>
+            <button className="largerButtons" onClick={() => clear()}>AC</button>
+            <button onClick={() => deleteLastDigit()}>DEL</button>
             <button className="operators">÷</button>
-            <button className="button">7</button>
-            <button>8</button>
-            <button>9</button>
+            <button onClick={() => Addigits('7')}>7</button>
+            <button onClick={() => Addigits('8')}>8</button>
+            <button onClick={() => Addigits('9')}>9</button>
             <button className="operators">×</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
+            <button onClick={() => Addigits('4')}>4</button>
+            <button onClick={() => Addigits('5')}>5</button>
+            <button onClick={() => Addigits('6')}>6</button>
             <button className="operators">-</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
+            <button onClick={() => Addigits("1")}>1</button>
+            <button onClick={() => Addigits('2')}>2</button>
+            <button onClick={() => Addigits('3')}>3</button>
             <button className="operators">+</button>
-            <button className="largerButtons">0</button>
-            <button>.</button>
+            <button className="largerButtons" onClick={() => Addigits('0')}>0</button>
+            <button onClick={() => Addigits('.')}>.</button>
             <button className="operators">=</button>
-        
         </div>
     )
 }
