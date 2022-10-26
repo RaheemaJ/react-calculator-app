bb873729120d9a4e7a68c13a0f8fc16580f354cc
RaheemaJ <112560174+RaheemaJ@users.noreply.github.com>
2022-10-26 15:40:59 +0100

    Delete calculator directory

--- calculator/src/Components/OperatorButtons.js
+++
@@ -1,7 +0,0 @@
-import {ACTIONS} from '../App'
-
-export default function OperatorButton({dispatch, digit}){
-    return (
-        <button onClick={() => dispatch( {type: ACTIONS.ADD_OPERATOR, payload: {digit}} )} className="operators">{digit}</button>
-        )
-}
